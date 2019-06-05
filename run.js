daa023c6b957142fe7ae8bdbdda69a7c6a433c0e
Camilo <capinzonr@unal.edu.co>
2019-06-05 15:45:27 -0500

    create docker

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 // var netanos = require('netanos');
 var netanos = require('./Netanos.js');
-var input = "Max and Ben spent more than 1000 hours on writing the software. They started in August 2016 in Amsterdam.";
-
+//var input = "Yo Steven Pardo, identificado con c.c. n° 1122334455 hace nueve años pisé una mina antipersonal en la finca de mis suegros. La explosión me levantó y caí de espaldas. Se me llenaron los ojos de tierra y cuando traté de pararme no pude porque mi pierna estaba mutilada. Me llevaron al hospital en donde estuve 15 días. Los médicos me evaluaron y el CICR me dio una prótesis especial para poder conducir mi camioneta y ganarme la vida transportando niños hacia el colegio donde estudian, en unas veredas del Putumayo.";
+var input = "Hijo de Gabriel Eligio García y Luisa Santiaga Márquez Iguarán, nació en Aracataca, departamento del Magdalena, Colombia, «el domingo 6 de marzo de 1928 a las nueve de la mañana...», como refiere el propio escritor en sus memorias.9​ Cuando sus padres se enamoraron, el padre de Luisa, el coronel Nicolás Ricardo Márquez Mejía, se opuso a esa relación, pues Gabriel Eligio García, que había llegado a Aracataca como telegrafista, no era el hombre que consideraba más adecuado para su hija, por ser hijo de madre soltera, pertenecer al Partido Conservador Colombiano y ser un mujeriego lo confeso.";
 var entities = {
     person: true,
     organization: true,
@@ -13,16 +13,16 @@ var entities = {
     other: true
 };
 
-netanos.anon(input, entities, function (output) {
+netanos.anon(input, entities, function(output) {
     console.log(output);
 });
 
-netanos.ner(input, entities, function (output) {
+netanos.ner(input, entities, function(output) {
     console.log(output);
 });
 
 console.log(netanos.noncontext(input));
 
-netanos.combined(input, entities, function (output) {
+netanos.combined(input, entities, function(output) {
     console.log(output);
 });
